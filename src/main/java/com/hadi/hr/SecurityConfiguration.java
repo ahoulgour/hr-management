0f1d66f7a9d47c5bab84753d5639e7e32f08a8c2
Gor <ahoulgour@gmail.com>
2020-03-28 23:00:34 +0100

    add swagger

--- src/main/java/com/hadi/hr/SecurityConfiguration.java
+++ src/main/java/com/hadi/hr/SecurityConfiguration.java
@@ -9,8 +9,13 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeRequests().antMatchers("/").permitAll().and()
-                .authorizeRequests().antMatchers("/console/**").permitAll();
+        httpSecurity.authorizeRequests()
+                .antMatchers("/")
+                .permitAll()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/h2-console/**")
+                .permitAll();
         httpSecurity.csrf().disable();
         httpSecurity.headers().frameOptions().disable();
     }
